afc63c01f6357402cf9d5a45f26bf852cf9bbd7b
petrowic-n <nemanjapetrovic358@gmail.com>
2025-10-31 17:43:33 +0100

    about section finished, services section started

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import "../styles/style.css";
 import "../styles/components/header.css";
 import "../styles/components/mobile-nav.css";
 import "../styles/components/hero.css";
+import "../styles/components/about.css";
+import "../styles/components/services.css";
 import "../styles/utils.css";
 
 import mobileNav from "./utils/mobile-nav";
